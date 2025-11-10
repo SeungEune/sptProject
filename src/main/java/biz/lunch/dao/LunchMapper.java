d7b71d1bf8bd0b167e9389bd4258a49fbeb586bf
Gunwoo <rkdrjsdn1000@gmail.com>
2025-11-10 14:06:40 +0900

    매퍼 추가

--- src/main/java/biz/lunch/dao/LunchMapper.java
+++ src/main/java/biz/lunch/dao/LunchMapper.java
@@ -13,6 +13,10 @@ public interface LunchMapper {
     int registerLunch(Map<String, Object> params) throws Exception;
 
     int updateLunch(Map<String, Object> params) throws Exception;
+    //수정 시 해당 내역의 참여자 목록 초기화
+    int deleteParticipantsByLunchId(int lunchId) throws Exception;
+    // 수정된 참여자 정보를 일괄 등록
+    int insertParticipants_Batch(Map<String, Object> params) throws Exception;
 
     int deleteLunch(int id) throws Exception;
 
