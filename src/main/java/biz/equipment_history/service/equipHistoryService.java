4137fb2b73534684c917b66e2875094974d2bf6e
seohyeon <iacsbe36@gmail.com>
2025-11-19 15:59:09 +0900

    chor: 충돌 해결

--- src/main/java/biz/equipment_history/service/equipHistoryService.java
+++
@@ -1,9 +0,0 @@
-package biz.equipment_history.service;
-
-import biz.equipment_history.dto.EquipHistoryResponse;
-
-import java.util.List;
-
-public interface equipHistoryService {
-    List<EquipHistoryResponse> getEquipHistories(Long equipId);
-}
